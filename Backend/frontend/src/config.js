f0e237533c255fd56c9e047ab472ce4ea38fff41
Charles Lane <charleslane23@gmail.com>
2020-08-18 18:19:24 -0400

    config

--- Backend/frontend/src/config.js
+++ Backend/frontend/src/config.js
@@ -1,8 +1,8 @@
 import React from 'react';
 const config = {
     // CHANGE DOMAIN FOR PRODUCTION
-    'domain': 'https://www.hflcharleston.com',
-   // 'domain': 'http://staging.yp8hg5fd3q.us-east-1.elasticbeanstalk.com',
+    // 'domain': 'https://www.hflcharleston.com',
+   'domain': 'http://staging.yp8hg5fd3q.us-east-1.elasticbeanstalk.com',
 //    'domain': 'http://localhost:8000',
     'agentsPath': 'agents',
     'shortText': 120,
